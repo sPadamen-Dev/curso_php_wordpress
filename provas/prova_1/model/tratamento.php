75afbc12ccb8bb07db17565df042051ea4d3e29a
Gilvan Rios <gilvanriosmoraes@gmail.com>
2024-02-18 22:23:42 -0300

    Criando layout prova

--- provas/prova_1/model/tratamento.php
+++ provas/prova_1/model/tratamento.php
@@ -1,4 +1,58 @@
 <?php
-include("")
+include("db_infor_da_vaga.php");
+
+class Tratamento{
+    public $search;
+    public $database;
+
+    public function __construct($search, $database){
+        $this -> search = $search;
+        $this -> database = $database;
+
+    }
+
+    /**
+     * Get the value of search
+     */ 
+    public function getSearch()
+    {
+        return $this->search;
+    }
+
+    // metodos
+
+    public function procuraPorPalavra(){
+
+        for($i = 0; $i <= count($this -> database); $i++){
+            if($this->database[$i]["tecnologias"] == null){
+                return "Palavra nao encontrada";
+            }else{
+                foreach ($this->database[$i]["tecnologias"] as $palavra) {
+                    if($this -> search == $palavra){
+                        return $this -> database[$i];
+                    }
+            
+                };
+            }
+        }  
+    }
+
+    public function procuraPorEndereco(){
+
+        for($i = 0; $i <= count($this -> database); $i++){
+            if(array_search($this->search, $this -> database[$i])){
+                return $this -> database[$i];
+            };
+        }
+        return "Nenhuma vaga nessa localidade";
+    }
+
+
+
+}
+
+$requisicao = new Tratamento("php", $bd_infor_das_vagas);
+$resultado = $requisicao->procuraPorPalavra();
+var_dump($resultado);
 
 ?>
